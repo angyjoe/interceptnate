761c42d7d1c80c6f35784bc0f9e6ca37f449824e
angyjoe <angyjoe@gmail.com>
2014-04-12 16:38:38 +0200

    Adding the DAOService.

---
+++ samples/mainsample/src/info/sarihh/interceptnate/samples/mainsample/daos/DAOService.java
@@ -0,0 +1,9 @@
+package info.sarihh.interceptnate.samples.mainsample.daos;
+
+/**
+ * @author Sari Haj Hussein
+ */
+public class DAOService {
+	public static final TraditionalStudentDAO traditionalDAO = new TraditionalStudentDAO();
+	public static final CallbackStudentDAO callbackDAO = new CallbackStudentDAO();
+}
