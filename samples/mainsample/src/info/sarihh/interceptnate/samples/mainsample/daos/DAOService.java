f4963255c74be00c764dffd3e6571053c5d37cc2
Sari Haj Hussein <angyjoe@gmail.com>
2015-05-03 19:01:45 +0200

    Updating the project.

--- samples/mainsample/src/info/sarihh/interceptnate/samples/mainsample/daos/DAOService.java
+++
@@ -1,9 +0,0 @@
-package info.sarihh.interceptnate.samples.mainsample.daos;
-
-/**
- * @author Sari Haj Hussein
- */
-public class DAOService {
-	public static final TraditionalStudentDAO traditionalDAO = new TraditionalStudentDAO();
-	public static final CallbackStudentDAO callbackDAO = new CallbackStudentDAO();
-}
--- src/info/sarihh/interceptnate/TransactionCallback.java
+++
@@ -1,8 +0,0 @@
-package info.sarihh.interceptnate;
-
-/**
- * @author Sari Haj Hussein
- */
-public interface TransactionCallback {
-	public <T> T doInTransaction();
-}
